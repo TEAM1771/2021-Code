bf4e37624215671b96291f6b6ba1500767f89a30
DavidPopesc <80603759+DavidPopesc@users.noreply.github.com>
2021-06-07 17:50:53 -0400

    New Swerve
    in progress

--- src/main/cpp/Drivetrain.cpp
+++ src/main/cpp/Drivetrain.cpp
@@ -1,50 +1,42 @@
 #include "drivetrain.hpp"
 #include <thread>
+#include <frc/geometry/Translation2d.h>
+#include <frc/kinematics/SwerveDriveKinematics.h>
+#include <frc/kinematics/SwerveModuleState.h>
+#include <frc/kinematics/ChassisSpeeds.h>
 // #include <cmath>
-Drivetrain::Drivetrain()
-{
-    navx->ZeroYaw();
-    navx->GetYaw();
-}
 
-void Drivetrain::gotoZero()
-{
-    for(auto&& wheel : wheels)
-        wheel->drive({{0,0,0},0});
-}
-void Drivetrain::goto180()
-{
-    for(auto&& wheel : wheels)
-        wheel->drive({{0,1,0},0});
-}
+// Locations for the swerve drive modules relative to the robot center.
+frc::Translation2d m_frontLeftLocation{0.381_m, 0.381_m};
+frc::Translation2d m_frontRightLocation{0.381_m, -0.381_m};
+frc::Translation2d m_backLeftLocation{-0.381_m, 0.381_m};
+frc::Translation2d m_backRightLocation{-0.381_m, -0.381_m};
 
-// get robot orientation from rio / navX
-double Drivetrain::get_angle()
-{
-    return 0;// -ngr::deg2rad(navx->GetYaw());
-}
-// drive in specified direction
-void Drivetrain::drive(Twist_I const& twist)
-{
-    Twist_R const twist_r { twist, get_angle() };
-    // std::cout << "twist: " << twist_r << '\n';
-    // std::cout  << "Yaw: " << twist_r.dtheta << '\n';
-    // std::vector<std::thread> t;
-    // for(auto&& wheel : wheels)
-    //     t.emplace_back([&] { wheel->drive(twist_r); }); //We believe this removes multithreading...
-    // for(auto& ts : t)
-    // {
-    //     ts.join();
-    // }
-    for(auto&& wheel : wheels)
-        wheel->drive(twist_r);
-}
+// Creating my kinematics object using the module locations.
+frc::SwerveDriveKinematics<4> m_kinematics{
+  m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation,
+  m_backRightLocation};
 
-void Drivetrain::print()
-{
-    int i = 0;
-    for(auto& wheel : wheels)
+// Example chassis speeds: 1 meter per second forward, 3 meters
+// per second to the left, and rotation at 1.5 radians per second
+// counterclockwise.
+frc::ChassisSpeeds speeds{1_mps, 3_mps, 1.5_rad_per_s};
 
-    std::cout << "Wheel " << ++i << ": " << ngr::rad2deg(wheel->get_angle() + wheel->alpha + wheel->beta) << '\n';
-    // std::cout << "Twist R is: " << Wheel::get_vector_for() << '\n';
-} 
+// Convert to module states. Here, we can use C++17's structured
+// bindings feature to automatically split up the array into its
+// individual SwerveModuleState components.
+// auto [fl, fr, bl, br] = kinematics.ToSwerveModuleStates(speeds);
+
+auto flOptimized = frc::SwerveModuleState::Optimize(fl, units::radian_t(m_turningEncoder.GetDistance()));
+
+// The desired field relative speed here is 2 meters per second
+// toward the opponent's alliance station wall, and 2 meters per
+// second toward the left field boundary. The desired rotation
+// is a quarter of a rotation per second counterclockwise. The current
+// robot angle is 45 degrees.
+frc::ChassisSpeeds speeds = frc::ChassisSpeeds::FromFieldRelativeSpeeds(
+  2_mps, 2_mps, units::radians_per_second_t(wpi::math::pi / 2.0), frc::Rotation2d(45_deg));
+
+
+// Now use this in our kinematics
+auto [fl, fr, bl, br] = frc::kinematics.ToSwerveModuleStates(speeds);
