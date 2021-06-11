6edf4661816f91ec963ed9ad5617aa6fb33b4cf2
DavidPopesc <80603759+DavidPopesc@users.noreply.github.com>
2021-06-10 22:25:53 -0400

    Ryan plz help

--- src/main/cpp/Drivetrain.cpp
+++ src/main/cpp/Drivetrain.cpp
@@ -25,7 +25,7 @@ frc::ChassisSpeeds speeds{1_mps, 3_mps, 1.5_rad_per_s};
 // Convert to module states. Here, we can use C++17's structured
 // bindings feature to automatically split up the array into its
 // individual SwerveModuleState components.
-// auto [fl, fr, bl, br] = kinematics.ToSwerveModuleStates(speeds);
+auto [fl, fr, bl, br] = m_kinematics.ToSwerveModuleStates(speeds);
 
 auto flOptimized = frc::SwerveModuleState::Optimize(fl, units::radian_t(m_turningEncoder.GetDistance()));
 
@@ -37,9 +37,8 @@ auto flOptimized = frc::SwerveModuleState::Optimize(fl, units::radian_t(m_turnin
 frc::ChassisSpeeds speeds = frc::ChassisSpeeds::FromFieldRelativeSpeeds(
   2_mps, 2_mps, units::radians_per_second_t(wpi::math::pi / 2.0), frc::Rotation2d(45_deg));
 
-
 // Now use this in our kinematics
-auto [fl, fr, bl, br] = kinematics.ToSwerveModuleStates(speeds);
+auto [fl, fr, bl, br] = m_kinematics.ToSwerveModuleStates(speeds);
 
 // Example module States
 frc::SwerveModuleState frontLeftState{23.43_mps, frc::Rotation2d(-140.19_deg)};
@@ -50,5 +49,17 @@ frc::SwerveModuleState backRightState{54.08_mps, frc::Rotation2d(-70.56_deg)};
 // Convert to chassis speeds. Here, we can use C++17's structured bindings
 // feature to automatically break up the ChassisSpeeds struct into its
 // three components.
-auto [forward, sideways, angular] = kinematics.ToChassisSpeeds(
+auto [forward, sideways, angular] = m_kinematics.ToChassisSpeeds(
+  frontLeftState, frontRightState, backLeftState, backRightState);
+
+// Example module States
+frc::SwerveModuleState frontLeftState{23.43_mps, frc::Rotation2d(-140.19_deg)};
+frc::SwerveModuleState frontRightState{23.43_mps, frc::Rotation2d(-39.81_deg)};
+frc::SwerveModuleState backLeftState{54.08_mps, frc::Rotation2d(-109.44_deg)};
+frc::SwerveModuleState backRightState{54.08_mps, frc::Rotation2d(-70.56_deg)};
+
+// Convert to chassis speeds. Here, we can use C++17's structured bindings
+// feature to automatically break up the ChassisSpeeds struct into its
+// three components.
+auto [forward, sideways, angular] = m_kinematics.ToChassisSpeeds(
   frontLeftState, frontRightState, backLeftState, backRightState);
