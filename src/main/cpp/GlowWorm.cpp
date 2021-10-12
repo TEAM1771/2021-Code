9ea9461218d6d7bc4cf49d0a5aaf26b87d415267
Ryan McHale <mrryanmchale@outlook.com>
2021-10-12 12:44:21 -0400

    Revert "New code files for GlowWorm"
    This reverts commit 7f6de7ee16a2b17cbd9cb63528eed88cc7f015cf.

--- src/main/cpp/GlowWorm.cpp
+++
@@ -1,9 +0,0 @@
-#include "GlowWorm.hpp"
-#include <iostream>
-// File (and corresponding HPP file) created by Kalen Cole (RadiantRanger26) at 10/11/21 18:23:30
-// Reference: 
-// PhotonVision documentation: https://docs.photonvision.org/en/latest/
-// GlowWorm documentation:     https://gloworm.vision/
-
-// I haven't actually written anything here yet (obviously) but hopefully we can just copy everything over from LimeLight.cpp/.hpp
-
