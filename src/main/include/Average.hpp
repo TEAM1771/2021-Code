9389f9015517f1bef1a89ca13dcc35f6de8d62ac
Joseph <16584585+redness360@users.noreply.github.com>
2021-10-21 18:22:59 -0400

    Implemented Average class for Photon data

---
+++ src/main/include/Average.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+class Average
+{ //Class since we might use this multiple times & it needs a static dataSet
+//    std::vector<double> dataSet;
+//    std::vector<double>::iterator iterator = dataSet.end();
+      double dataSet[5];
+      int iterator = 0;
+      int sizeOfDataSet = 0;
+
+public:
+    double average(double input);
+};
