31bd323702302121819ab8e42c34363cc6686dc9
MrRedness <16584585+MrRedness@users.noreply.github.com>
2021-12-29 02:14:22 -0500

    Average.hpp clean-up & logic assertion

--- src/main/include/Average.hpp
+++ src/main/include/Average.hpp
@@ -1,34 +1,114 @@
 #pragma once
 #include <iostream>
-#include <vector>
+// #include <vector>
+#include <assert.h>
 
 template <int size>
 class Average
-{   //Class since we might use this multiple times & it needs a static dataSet
-    //    std::vector<double> dataSet;
-    //    std::vector<double>::iterator iterator = dataSet.end();
-    double dataSet[size];
-    int    iterator      = 0;
-    int    sizeOfDataSet = 0;
+{
+    double data_set[size];
+    int iterator = 0;
+    int size_of_data_set = 0;
 
 public:
-    constexpr double operator()(double input)
+    Average()
     {
-        if(iterator == size)
+        assert(testLogic());
+    }
+    void clear()
+    {
+        int iterator = 0;
+        int size_of_data_set = 0;
+    }
+    double operator()(double input)
+    {
+        if (iterator == size)
         {
             iterator = 0;
         }
-        if(sizeOfDataSet < size)
+        if (size_of_data_set < size)
         {
-            sizeOfDataSet++;
+            size_of_data_set++;
         }
-        dataSet[iterator] = input;
+        data_set[iterator] = input;
         iterator++;
         double sum = 0;
-        for(int i = 0; i < sizeOfDataSet; i++)
+        for (int i = 0; i < size_of_data_set; i++)
         {
-            sum += dataSet[i];
+            sum += data_set[i];
         }
-        return sum / sizeOfDataSet;
+        return sum / size_of_data_set;
+    }
+        bool testLogic() const
+    {
+        double test_data_set[3]{2234.69, 5648415, -.14};
+        int test_size = 3;
+        int test_iterator = 0;
+        int size_of_test_data_set = 0;
+        auto avg = [&test_data_set, &test_iterator, &size_of_test_data_set, &test_size](double input)
+        {
+            if (test_iterator >= test_size)
+            {
+                test_iterator = 0;
+            }
+            if (size_of_test_data_set < test_size)
+            {
+                size_of_test_data_set++;
+            }
+            test_data_set[test_iterator] = input;
+            test_iterator++;
+            double sum = 0;
+            for (int i = 0; i < size_of_test_data_set; i++)
+            {
+                sum += test_data_set[i];
+            }
+            return sum / size_of_test_data_set;
+        };
+
+        double expected_value;
+        for (int i = 0; i < 10; i++)
+        {
+            double observed_value = avg(i);
+            if (i == 0)
+                expected_value = 0;
+            else if (i == 1)
+                expected_value = 0.5;
+            else
+                expected_value = i - 1;
+
+            if (observed_value != expected_value)
+            {
+                std::cout << "Error while testing Average logic: Expected Value: " << expected_value << " , Observed: " << observed_value << ".\n";
+                std::cout << "Current value in test_data_set[0] = " << test_data_set[0] << ", [1] = " << test_data_set[1] << ", [2] = " << test_data_set[2] << ".\n";
+                std::cout << "Current value of test_iterator = " << test_iterator << ", size_of_test_data_set = " << size_of_test_data_set << ".\n";
+                return false;
+            }
+        }
+        // if (avg(0) != 0){
+        //     std::cout << +
+        //     return false;
+        // }
+        // if (avg(1) != 0.5)
+        //     return false;
+        // if (avg(2) != 1)
+        //     return false;
+        // if (avg(3) != 2)
+        //     return false;
+        // if (avg(4) != 3)
+        //     return false;
+        // if (avg(5) != 4)
+        //     return false;
+        // if (avg(6) != 5)
+        //     return false;
+        // if (avg(7) != 6)
+        //     return false;
+        return true;
     }
 };
+
+// template <int size>
+// class AverageWithAssert
+// {
+// public:
+//     static Average<size> avg;
+// };
