5063f35783393fc656b1c35e25bb1002d9e6681c
Joseph <16584585+redness360@users.noreply.github.com>
2021-12-30 02:42:11 -0500

    Still goin

--- src/main/cpp/Hopper.cpp
+++ src/main/cpp/Hopper.cpp
@@ -1,6 +1,7 @@
 #include "Hopper.hpp"
 #include "ShooterWheel.hpp"
 
+#include <rev/CANSparkMax.h>
 #include <frc/DigitalInput.h>
 
 /******************************************************************/
