f02ddf38f325f90c3bc5654ce5632ef4c8f028f5
Joseph <16584585+redness360@users.noreply.github.com>
2021-12-30 02:07:34 -0500

    Seperated into constexpr & nonconstexpr version

--- src/main/include/Average.hpp
+++ src/main/include/Average.hpp
@@ -2,30 +2,32 @@
 #include <array>
 #include <numeric>
 
-template <int max_size>
-auto RollingAverage = [data_set = std::array<double, max_size>(), i = 0, current_size = 0](double input) mutable -> double {
-    if(i == max_size)
-        i = 0;
-    if(current_size < max_size)
-        current_size++;
-    data_set[i++] = input;
-    return std::accumulate(data_set.begin(), data_set.end(), 0.0) / current_size;
-};
-
-template <int max_size>
-constexpr auto RollingAverageConstexpr = [data_set = std::array<double, max_size>(), i = 0, current_size = 0](double input) mutable -> double {
-    if(i == max_size)
-        i = 0;
-    if(current_size < max_size)
-        current_size++;
-    data_set[i++] = input;
-    double sum    = 0.00;
-    for(int i = 0; i < current_size; i++)
-        sum += data_set[i];
-    return sum / current_size;
-};
-
 namespace RollingAvg
 {
-    constexpr bool testLogic();
-}
+    template <int max_size>
+    auto avg = [data_set = std::array<double, max_size>(), i = 0, current_size = 0](double input) mutable -> double {
+        if(i == max_size)
+            i = 0;
+        if(current_size < max_size)
+            current_size++;
+        data_set[i++] = input;
+        return std::accumulate(data_set.begin(), data_set.end(), 0.0) / current_size;
+    };
+
+    template <int max_size>
+    constexpr auto constexprAvg = [data_set = std::array<double, max_size>(), i = 0, current_size = 0](double input) mutable -> double {
+        if(i == max_size)
+            i = 0;
+        if(current_size < max_size)
+            current_size++;
+        data_set[i++] = input;
+        double sum    = 0.00;
+        for(int i = 0; i < current_size; i++)
+            sum += data_set[i];
+        return sum / current_size;
+    };
+
+    bool           testLogic();
+    constexpr bool testConstexprLogic();
+
+} // namespace RollingAvg
